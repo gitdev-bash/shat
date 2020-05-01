8bf42d69baf88bf6ec410508b1569acaa4ca9df4
gitdev <63194733+gitdev-bash@users.noreply.github.com>
2020-05-01 20:29:34 +0300

    Update shat.sh

--- shat.sh
+++ shat.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Simple encrypted network chat
 # license WTFPL
-
+# Check for dependency
+check_nc=$(which nc)
+[ -z "$check_nc" ] && echo "You don`t have nc installed.Install nc and try again" && c1="false" || c1="true"
+check_nmap=$(which nmap)
+[ -z "$check_nmap" ] && echo "You don`t have nmap installed.Install nmap and try again" && c2="false" || c2="true"
+check_openssl=$(which openssl)
+[ -z "$check_openssl" ] && echo "You don`t have openssl installed.Install openssl and try again" && c3="false" || c3="true"
+[ "$c1" == "true" -a "$c2" == "true" -a "$c3" == "true" ] && exit
 port="31415"
 ip="$1"
 name="$USER"
@@ -21,6 +28,7 @@ decrypt () {
 export -f decrypt
 
 open=$(nmap -sT $ip -p $port  | grep "$port/tcp.*open")
+#nmap is not instaled by default on manny systems
 
 if [ "$open" ]
 then
